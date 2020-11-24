ebd4ec3fc7b0e53c08192cd5e8fbdf6c4c4feded
wyh <wyhuii@126.com>
2020-11-24 22:05:52 +0800

    统一异常处理

--- wyh-xinguan-base-web/src/main/java/com/wyh/system/controller/TbUserController.java
+++ wyh-xinguan-base-web/src/main/java/com/wyh/system/controller/TbUserController.java
@@ -1,12 +1,15 @@
 package com.wyh.system.controller;
 
 
+import com.wyh.handler.BusinessException;
+import com.wyh.response.ResultCode;
 import com.wyh.system.entity.TbUser;
 import com.wyh.system.service.TbUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.wyh.response.Result;
 
@@ -34,8 +37,21 @@ public class TbUserController {
     @GetMapping
     @ApiOperation(value = "用户列表",notes="查询所有用户信息")
     public Result findUsers(){
+        //int i = 1/0;
         List<TbUser> list = tbUserService.list();
         return Result.ok().data("users",list);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "查询单个用户",notes="通过ID查询对应的用户信息")
+    public Result getUserById(@PathVariable("id") Long id){
+        TbUser user = tbUserService.getById(id);
+        if(user!=null){
+            return Result.ok().data("user",user);
+        }else{
+            throw new BusinessException(ResultCode.USER_NOT_FOUND_EXCEPTION.getCode(), ResultCode.USER_NOT_FOUND_EXCEPTION.getMessage());
+        }
+
     }
 
 }
